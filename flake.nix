d696dc4f1a16cfc1dc3f913c1ff05bc320303264
Raymond Dean III <rdean@lifeatvictory.com>
2026-04-07 16:53:16 -0400

    Update structure

--- flake.nix
+++ flake.nix
@@ -110,29 +110,5 @@
         ];
       };
     };
-      bootstrap = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-            ({ config, ... }: {
-              # shut up state version warning
-              nixpkgs.system = "x86_64-linux";
-              system.stateVersion = "24.11";
-              # Adjust this to your liking.
-              # WARNING: if you set a too low value the image might be not big enough to contain the nixos installation
-            })
-            disko.nixosModules.disko
-            ({ config, ... }: {
-              # shut up state version warning
-              # Adjust this to your liking.
-              disko.devices.disk.system.device = "/dev/sda";
-              boot.loader.grub.device = "/dev/sda";
-              # WARNING: if you set a too low value the image might be not big enough to contain the nixos installation
-              #disko.devices.disk.system.imageSize = "10G";
-            })
-            ./disk-config/mac-mini.nix
-            ./service-config/UI/gnome.nix
-            ./base-config/mac-mini.nix # Base system config. Meant to be extended with below lines.
-        ];
-      };
-    };
-  }
+  };
+}
