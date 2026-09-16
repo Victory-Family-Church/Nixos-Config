0fdcb9328ec938c36aff1f36e066fcb98f7b9c81
pcs3rd <18014111+pcs3rd@users.noreply.github.com>
2026-09-16 19:11:53 -0400

    this got me

--- service-config/docker/containerd.nix
+++ service-config/docker/containerd.nix
@@ -2,7 +2,7 @@
   virtualisation.docker.enable = true; # Enable oci containers.
   virtualisation.oci-containers.backend = "docker"; # Make sure we're actually using docker, and not podman.
   virtualisation.docker.liveRestore = false; # This breaks swarms
-  virtualisation.docker.daemon.settings = { # Set Docker-Daemon path to non-tmpfs location, /docker-data is a btrfs vol
-    data-root = "/docker-data";
-  };
+#  virtualisation.docker.daemon.settings = { # Set Docker-Daemon path to non-tmpfs location, /docker-data is a btrfs vol
+#    data-root = "/docker-data";
+#  };
 }
