cf05495b4f0f361625e7d1f1253f4cb4045133c8
pcs3rd <18014111+pcs3rd@users.noreply.github.com>
2026-09-16 16:07:51 -0400

    kernel mods

--- base-config/resi-aux.nix
+++ base-config/resi-aux.nix
@@ -21,7 +21,7 @@
     };
 
 
-    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "uas" "usbhid" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
     boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
